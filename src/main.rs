671c819a5319e43fb21091eba079bfb0ea63ea76
Pieter Partous <pieter.partous@outlook.com>
2022-11-16 20:01:53 +0100

    Move type definitions to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,13 @@
-use clap::Parser;
-use std::{str::FromStr, fs};
 mod utils;
-use utils::{PerkName, WantedGizmo, Perk};
-
-#[derive(Parser, Debug)]
-#[command(author, version, about)]
-struct Args {
-    /// Perk to look for
-    perk: String,
-
-    /// Rank of the first perk
-    #[arg(default_value_t = 1)]
-    rank: u8,
-
-    /// Second perk in the gizmo
-    perk_two: Option<String>,
-
-    /// Rank of the second perk
-    #[arg(default_value_t = 1)]
-    rank_two: u8,
-
-    #[arg(value_enum, short('t'), long("type"))]
-    gizmo_type: utils::GizmoType,
-
-    /// Invention level. Use two values separated by a comma to search in a range
-    #[arg(short('l'), long("level"), required(true), use_value_delimiter = true, value_delimiter = ',')]
-    invention_level: Vec<u32>,
-
-    /// Is ancient gizmo
-    #[arg(short, long)]
-    ancient: bool,
-
-    /// Use this if you don't care what the second perk is
-    #[arg(short, long)]
-    fuzzy: bool,
-
-    /// Comma separated list of material values to exclude. Uses basic substring matching.
-    #[arg(short, long, use_value_delimiter = true, value_delimiter = ',')]
-    exclude: Vec<String>,
-
-    /// Sort the result on probability per consumed gizmo, probability per attempt, or on estimated price.
-    #[arg(value_enum, short, long, default_value_t = utils::SortType::Price)]
-    sort_type: utils::SortType
-}
+mod definitions;
+use std::{str::FromStr, fs};
+use clap::Parser;
+use definitions::*;
 
 fn main() {
     let args = Args::parse();
     let data = fs::read_to_string("data.json").unwrap();
-    let data: utils::Data = serde_json::from_str(&data).unwrap();
+    let data: Data = serde_json::from_str(&data).unwrap();
     let wanted_gizmo = process_wanted_gizmo(&args, &data);
 
     validate_input(&args, &wanted_gizmo, &data);
@@ -56,7 +16,7 @@ fn main() {
     println!("{:#?}", wanted_gizmo);
 }
 
-fn process_wanted_gizmo(args: &Args, data: &utils::Data) -> WantedGizmo {
+fn process_wanted_gizmo(args: &Args, data: &Data) -> WantedGizmo {
     let perk_one = PerkName::from_str(&args.perk).unwrap_or_else(|_| {
         utils::print_error(format!("Perk '{}' does not exist.", &args.perk).as_str())
     });
@@ -83,7 +43,7 @@ fn process_wanted_gizmo(args: &Args, data: &utils::Data) -> WantedGizmo {
     )
 }
 
-fn validate_input(args: &Args, wanted_gizmo: &WantedGizmo, data: &utils::Data) {
+fn validate_input(args: &Args, wanted_gizmo: &WantedGizmo, data: &Data) {
     if wanted_gizmo.0.doubleslot && wanted_gizmo.1.perk != PerkName::Empty {
         utils::print_error(format!("Perk '{}' can't be combined with another perk as it uses both slots.", wanted_gizmo.0.perk).as_str())
     }
@@ -106,17 +66,15 @@ fn validate_input(args: &Args, wanted_gizmo: &WantedGizmo, data: &utils::Data) {
     match &args.invention_level.len() {
         1 => {
             match &args.invention_level[0] {
-                0 => utils::print_error("Invention level must be greater than 0."),
-                n if *n > 137 => utils::print_error("Invention level must be between 1 and 137."),
-                _ => ()
+                1..=137 => (),
+                _ => utils::print_error("Invention level must be between 1 and 137.")
             }
         },
         _ => {
-            match &args.invention_level[0..2] {
-                [0, _] | [_, 0]  => utils::print_error("Invention level must be greater than 0."),
-                [n, _] | [_, n] if *n > 137 => utils::print_error("Invention level must be between 1 and 137."),
+            match &args.invention_level[0..=1] {
                 [x, y] if x > y => utils::print_error("First value of the invention level range must be lower or equal to the second value."),
-                _ => ()
+                [1..=137, 1..=137] => (),
+                _ => utils::print_error("Invention level must be between 1 and 137.")
             }
         }
     }
