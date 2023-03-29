00c3a469c55a0dffda51e13af7b873bb64fe2f00
Pieter Partous <pieter.partous@outlook.com>
2023-03-29 13:38:55 +0200

    prep for gui

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 cargo build --release
-Copy .\target\release\perk_solver.exe .\perk_solver.exe
+Copy .\target\release\perk_solver_cli.exe .\perk_solver.exe
 Copy .\target\release\perk_solver.dll .\perk_solver.dll
