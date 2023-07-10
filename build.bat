75bbe4ac49ca8d6f4f81dd9267eefc4e0d3c4e5d
Pieter Partous <pieter.partous@outlook.com>
2023-07-10 15:53:18 +0200

    Update dependencies

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 cargo build --release --bin perk_solver_cli
 Copy .\target\release\perk_solver_cli.exe .\perk_solver.exe
-Copy .\target\release\perk_solver.dll .\perk_solver.dll
 
 cargo build --release --bin perk_solver_gui --features="gui"
 Copy .\target\release\perk_solver_gui.exe .\perk_solver_gui.exe
--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 cargo build --release
 cp ./target/release/perk_solver_cli ./perk_solver
-cp ./target/release/libperk_solver.so ./perk_solver.so
